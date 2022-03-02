e9ed1a919a8005f512df43a76c5670a23aaf9098
KimGiTae <rlxo6919@naver.com>
2022-03-02 10:46:02 +0900

    한국어 decode

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ const s3 = new AWS.S3();
 
 exports.handler = async (event, context, callback) => {
   const Bucket = event.Records[0].s3.bucket.name;
-  const Key = event.Records[0].s3.object.key;
-
+  const Key = decodeURIComponent(event.Records[0].s3.object.key);
+  console.log(Bucket, Key);
   const filename = Key.split('/')[Key.split('/').length - 1];
   const ext = Key.split('.')[Key.split('.').length - 1];
   const requiredFormat = ext === 'jpg' ? 'jpeg' : ext; // sharp에서는 jpg 대신 jpeg 사용합니다.
   console.log('name', filename, 'ext', ext);
 
   try {
-    console.log('Key', Key);
     const s3Object = await s3.getObject({ Bucket, Key }).promise(); // 버퍼로 가져오기
     console.log('original', s3Object.Body.length);
     const resizedImage = await sharp(s3Object.Body) // 리사이징
